ae9258d3a60530b21bbe50590bc42be2f6df40b8
taka <tomo05061@gmail.com>
2021-03-25 00:22:37 +0900

    fix: SkillCardを修正

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
 const View = (props: SkillCard) => {
+
+  // 星で熟練度を表現
+  const generateStars = ({ level }: SkillCard) => {
+    const stars = [];
+
+    for (let i = 0; i < level; i++) {
+      stars.push(<li>★</li>);
+    }
+
+    return stars;
+  };
+
   return (
     <div>
       <h2>{props.skillName}</h2>
       <img src={props.skillImg} alt={`${props.skillName}の画像`} />
-      <ul>
-        {[...new Array(props.level)].map((lang) => (
-          <li>★</li>
-        ))}
-      </ul>
+      <ul>{generateStars(props)}</ul>
       <p>{props.contents}</p>
     </div>
   );
